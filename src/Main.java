5e2365e5dead72959a29907c480c69f3085a8d8a
Alexandre Borges <alexandreborges.goncalves@gmail.com>
2022-08-15 15:06:16 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,10 +23,38 @@ public class Main {
         mentoria.setDescricao("Para quem esta comecando a carreira em java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp voltado para desenvolvedores Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev devAlexandre = new Dev();
+        devAlexandre.setNome("Alexandre");
+        devAlexandre.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + devAlexandre.getConteudosInscritos());
+        devAlexandre.progredir();
+        devAlexandre.progredir();
+        System.out.println("---");
+        System.out.println("Conteudos inscritos: " + devAlexandre.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: " + devAlexandre.getConteudosConcluidos());
+        System.out.println("XP:" + devAlexandre.calcularTotalXp());
+
+        System.out.println("-----------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("----");
+        System.out.println("Conteudos inscritos: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
+
+
+
 
     }
 }
